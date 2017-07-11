3bb5004a7d83f12b15284f754f0c107fd8881bad
Zach <zach@codehooker.com>
2017-07-11 17:23:29 -0400

    moves the basic config out of the class script, adds sample.

--- cybot.py
+++ cybot.py
@@ -3,8 +3,7 @@ import requests as req
 from msg import Msg
 import threading
 from foaas import fuck
-from urllib.parse import urlparse
-from socketIO_client import SocketIO, BaseNamespace
+from socketIO_client import BaseNamespace
 
 
 def check_init(func):
@@ -165,18 +164,3 @@ class Client(BaseNamespace):
 
     def on_mediaUpdate(self, *args):
         pass
-
-
-def connect(channel, username, password):
-    url = 'http://%s/socketconfig/%s.json' % ('cytu.be', channel)
-    server_list = req.get(url).json()['servers']
-    server = [i['url'] for i in server_list if i['secure'] is False][0]
-    url = urlparse(server)
-    sio = SocketIO(url.hostname, url.port, Client)
-    instance = sio.get_namespace()
-    instance.login(channel, username, password)
-    sio.wait()
-
-
-if __name__ == '__main__':
-    connect('chan', 'username', 'password')
