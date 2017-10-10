413fa98e2f21f0ce1fbc22d376171e60c89d2a1b
Zach <zach@codehooker.com>
2017-10-09 22:25:16 -0400

    fixes list out of range for slots

--- cybot/bot.py
+++ cybot/bot.py
@@ -121,7 +121,7 @@ class Client(BaseNamespace):
                 x = int(random.triangular(0, 6, 2))
                 z = int(random.triangular(0, 6, 2))
                 y = int(random.triangular(0, 6, 2))
-                translate = ['🍇', '🍈', '🍋', '🂻', '♦']
+                translate = ['🍇', '🍈', '🍋', '🍌', '🂻', '♦']
                 self.sendmsg('| {} | {} | {} |'.format(
                     translate[x], translate[y], translate[z]))
                 if 5 in (x, y, z) and (x == y == z):
